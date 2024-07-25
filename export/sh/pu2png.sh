6d63853adcd459bc2543d965b3b137d71918849f
ichiroprogrammer <160194260+ichiroprogrammer@users.noreply.github.com>
2024-07-25 18:28:34 +0900

    add command pu to png

---
+++ export/sh/pu2png.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+readonly BASE_DIR=$(cd $(dirname $0); pwd)
+readonly BASENAME="$(basename $0)"
+
+readonly PY_CMD="${BASE_DIR}/../py/plant_uml_encode.py"
+
+function help(){
+    local -r exit_code=$1
+    set +x
+
+    echo "$BASENAME  <XXX. pu> : XXX. pu to XXX.pnge"
+    echo " <XXX.pu>           : plant uml code file"
+    echo "    -h              : show this message"
+
+    exit $exit_code
+}
+
+while getopts "xh" flag; do
+    case $flag in 
+    x)  set -x ;; 
+    h)  help 0 ;; 
+    \?) help 1 ;; 
+    esac
+done
+shift $(expr ${OPTIND} - 1)
+
+readonly PU_FILE=$1
+PNG_FILE="${PU_FILE%.pu}.png"
+
+$PY_CMD $PU_FILE -o $PNG_FILE
+
