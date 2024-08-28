1eb0bd1bb7c966b9a8c4c7b75ea22a8708c81a68
ichiroprogrammer <160194260+ichiroprogrammer@users.noreply.github.com>
2024-08-28 14:08:37 +0900

    refactored test

--- test/test_sh.sh
+++ test/test_sh.sh
@@ -3,11 +3,11 @@
 declare -r BASE_DIR=$(cd $(dirname $0); pwd)
 
 cd $BASE_DIR
-$BASE_DIR/../export/sh/md_to_html.sh -H -x -o tako.md sh_data/single.md 
+$BASE_DIR/../export/sh/md_to_html.sh -H -x -o act.md sh_data/single.md 
 
-diff sh_data/md_to_html_exp.md tako.md
-diff sh_data/md_to_html_exp.html tako.html
+diff sh_data/md_to_html_exp.md act.md
+diff sh_data/md_to_html_exp.html act.html
 
-rm tako.md
+rm act.md act.html
 
 
