17b5184eefbde2d0e97aef8199fec64e2fba0887
ichiroprogrammer <160194260+ichiroprogrammer@users.noreply.github.com>
2025-01-15 16:27:09 +0900

    local rendering for pu file

--- export/sh/pu2png.sh
+++ export/sh/pu2png.sh
@@ -1,9 +1,44 @@
-#!/bin/bash -e
+#!/bin/bash
 
 readonly BASE_DIR=$(cd $(dirname $0); pwd)
 readonly BASENAME="$(basename $0)"
+readonly PLANTUML="$BASE_DIR/plantuml.jar"
 
-readonly PY_CMD="${BASE_DIR}/../py/plant_uml_encode.py"
+function install_help() {
+    cat << EOS
+# on ubuntu in wsl
+# install env to run plantuml.jar
+
+sudo apt update
+
+sudo apt install openjdk-17-jdk -y      # install java
+sudo apt install graphviz -y
+sudo apt install plantuml -y
+wget https://github.com/plantuml/plantuml/releases/latest/download/plantuml.jar -O plantuml.jar
+
+EOS
+}
+
+
+function check_pkg() {
+
+    which java > /dev/null
+    if [[ $? -ne 0 ]]; then
+        install_help
+        exit 1
+    fi
+
+    which plantuml > /dev/null
+    if [[ $? -ne 0 ]]; then
+        install_help
+        exit 1
+    fi
+
+    if [[ ! -e $PLANTUML  ]]; then
+        install_help
+        exit 1
+    fi
+}
 
 function help(){
     local -r exit_code=$1
@@ -25,8 +60,12 @@ while getopts "xh" flag; do
 done
 shift $(expr ${OPTIND} - 1)
 
-readonly PU_FILE=$1
-PNG_FILE="${PU_FILE%.pu}.png"
+set -e
 
-$PY_CMD $PU_FILE -o $PNG_FILE
+readonly PU_FILE=$1
+readonly PNG_FILE="${PU_FILE%.pu}.png"
+
+check_pkg
+
+java -jar $PLANTUML $PU_FILE
 
